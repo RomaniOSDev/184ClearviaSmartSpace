f3b9d84fe612a761206a50976da075f7973d6d5b
Roman <romazaniosdev@gmail.com>
2026-05-24 12:05:17 +0300

    Initial Commit

---
+++ 184ClearviaSmartSpace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  184ClearviaSmartSpace
+//
+//  Created by Roman on 5/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
